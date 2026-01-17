efba330c55012c8525940bc67c9295b87aa13dec
Rizki Cahya <rizki.cahya03@gmail.com>
2026-01-17 14:47:42 +0700

    feat: responsive navbar & layout

--- navbar.php
+++ navbar.php
@@ -9,31 +9,36 @@ $current_page = isset($current_page) ? $current_page : '';
         <a class="navbar-brand" href="index.php">
             <i class="fas fa-museum me-2"></i>Artify
         </a>
-        <div class="navbar-nav ms-auto d-flex align-items-center">
-            <?php if ($current_page != 'index'): ?>
-                <a class="nav-link" href="index.php">
-                    <i class="fas fa-home me-1"></i>Beranda
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="navbar-nav ms-auto align-items-lg-center">
+                <?php if ($current_page != 'index'): ?>
+                    <a class="nav-link" href="index.php">
+                        <i class="fas fa-home me-1"></i>Beranda
+                    </a>
+                <?php endif; ?>
+                <a class="nav-link" href="review.php">
+                    <i class="fas fa-edit me-1"></i>Review
                 </a>
-            <?php endif; ?>
-            <a class="nav-link" href="review.php">
-                <i class="fas fa-edit me-1"></i>Review
-            </a>
-            <?php if (isset($_SESSION['user_name'])): ?>
-                <a class="nav-link" href="recommendations.php">
-                    <i class="fas fa-star me-1"></i>Rekomendasi
-                </a>
-                <span class="nav-link d-flex align-items-center pe-0">
-                    <i class="fas fa-user-circle me-2"></i>
-                    <span><?php echo htmlspecialchars($_SESSION['user_name']); ?></span>
-                </span>
-                <a class="nav-link" href="logout.php">
-                    <i class="fas fa-sign-out-alt me-1"></i>Logout
-                </a>
-            <?php else: ?>
-                <a class="nav-link" href="login.php">
-                    <i class="fas fa-sign-in-alt me-1"></i>Login
-                </a>
-            <?php endif; ?>
+                <?php if (isset($_SESSION['user_name'])): ?>
+                    <a class="nav-link" href="recommendations.php">
+                        <i class="fas fa-star me-1"></i>Rekomendasi
+                    </a>
+                    <span class="nav-link d-flex align-items-center pe-0">
+                        <i class="fas fa-user-circle me-2"></i>
+                        <span><?php echo htmlspecialchars($_SESSION['user_name']); ?></span>
+                    </span>
+                    <a class="nav-link" href="logout.php">
+                        <i class="fas fa-sign-out-alt me-1"></i>Logout
+                    </a>
+                <?php else: ?>
+                    <a class="nav-link" href="login.php">
+                        <i class="fas fa-sign-in-alt me-1"></i>Login
+                    </a>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 </nav>
